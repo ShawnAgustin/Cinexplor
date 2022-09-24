eaa5e3c61edf6a6d600208b2965c6bab320d8b8b
Shawn Agustin <sragusti@uci.edu>
2022-09-23 20:17:24 -0700

    carousel css centered

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,8 +58,35 @@ function Home() {
       speed: 500,
       slidesToShow: 3,
       autoplay: true,
+      swipeToSlide: true,
       centerMode: true,
       centerPadding: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ],
       nextArrow: <NextArrow />,
       prevArrow:<PrevArrow />,
       beforeChange: (current, next) => setImageIndex(next)
